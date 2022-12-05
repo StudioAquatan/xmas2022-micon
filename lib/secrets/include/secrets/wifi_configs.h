6b1960d543fb2a2384aba126e6b77fd63f5cd085
Hiroya-W <hyuyukun@gmail.com>
2022-12-05 11:10:54 +0900

    :recycle: Fix typo

--- lib/secrets/include/secrets/wifi_configs.h
+++ lib/secrets/include/secrets/wifi_configs.h
@@ -1,8 +1,8 @@
-#ifndef INCLUDE_GUARD_WIFI_CONFIG_H
-#define INCLUDE_GUARD_WIFI_CONFIG_H
+#ifndef INCLUDE_GUARD_WIFI_CONFIGS_H
+#define INCLUDE_GUARD_WIFI_CONFIGS_H
 
 // Wifi network details.
 extern const char WIFI_SSID[];
 extern const char WIFI_PASSWORD[];
 
-#endif  // INCLUDE_GUARD_WIFI_CONFIG_H
+#endif  // INCLUDE_GUARD_WIFI_CONFIGS_H
