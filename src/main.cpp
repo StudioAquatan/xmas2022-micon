cfd66fcd2e8c1de1c15e9231448878500ab9042d
Hiroya-W <hyuyukun@gmail.com>
2022-12-06 23:49:59 +0900

    :sparkles: Use multi core

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,11 @@
 unsigned long lastMillis = 0;
 volatile uint8_t gCurrentPatternNumber = 0;
 
+void mqtt_main_loop(void *arg);
+void led_main_loop(void *arg);
+
 void setup() {
+    delay(3000);
     Serial.begin(9600);
 
     sprintf(deviceShadowPublishTopic, "$aws/things/%s/shadow/update", THINGNAME);
@@ -28,32 +32,64 @@ void setup() {
     setupWiFi();
     setupAWSIoT();
     publishCurrentPatternNumber();
+
+    xTaskCreatePinnedToCore(
+        mqtt_main_loop,
+        "mqtt_main_loop",
+        8192,
+        NULL,
+        1,
+        NULL,
+        0);
+
+    xTaskCreatePinnedToCore(
+        led_main_loop,
+        "led_main_loop",
+        8192,
+        NULL,
+        2,
+        NULL,
+        1);
 }
 
 void loop() {
-    mqttClient.loop();
-
-    // WiFiの再接続を待って、MQTTを接続しにいく
-    if (WiFi.status() == WL_CONNECTED && !mqttClient.connected()) {
-        while (!mqttClient.connect(THINGNAME)) {
-            Serial.print(".");
-            delay(100);
-        }
-        Serial.println("");
-
-        if (!mqttClient.connected()) {
-            Serial.println("AWS IoT Timeout!");
-            return;
-        }
-    }
-
-    if (mqttClient.connected()) {
-        // publish a heatbeat roughly every second.
-        if (millis() - lastMillis > 60000) {
-            lastMillis = millis();
-            publishHeatbeat();
-        }
-    }
-
     delay(1000);
+}
+
+// void loop() {
+void mqtt_main_loop(void *arg) {
+    while (1) {
+        mqttClient.loop();
+
+        // WiFiの再接続を待って、MQTTを接続しにいく
+        if (WiFi.status() == WL_CONNECTED && !mqttClient.connected()) {
+            while (!mqttClient.connect(THINGNAME)) {
+                Serial.print(".");
+                delay(100);
+            }
+            Serial.println("");
+
+            if (!mqttClient.connected()) {
+                Serial.println("AWS IoT Timeout!");
+                return;
+            }
+        }
+
+        if (mqttClient.connected()) {
+            // publish a heatbeat roughly every second.
+            if (millis() - lastMillis > 60000) {
+                lastMillis = millis();
+                publishHeatbeat();
+            }
+        }
+
+        delay(1000);
+    }
+}
+
+void led_main_loop(void *arg) {
+    while (1) {
+        Serial.println("led_main_loop");
+        delay(1000);
+    }
 }
