50fbfc63d883415d57913124f34e2a9ba9a5b294
Hiroya-W <hyuyukun@gmail.com>
2022-12-13 14:10:51 +0900

    :tada: Update version

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "4"
+  #define BUILD_NUMBER "5"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.4 - 2022-12-13 12:11:33.931017"
+  #define VERSION "v0.1.5 - 2022-12-13 14:09:16.690194"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.4"
+  #define VERSION_SHORT "v0.1.5"
 #endif
