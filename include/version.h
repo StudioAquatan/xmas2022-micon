5eadd423844ecd1a0c2e1ee9b29a330f95f748e5
Hiroya-W <hyuyukun@gmail.com>
2022-12-14 17:28:22 +0900

    :+1: Fix NUM_LEDS for cafeteria tree

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "6"
+  #define BUILD_NUMBER "7"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.6 - 2022-12-13 21:40:27.999720"
+  #define VERSION "v0.1.7 - 2022-12-14 17:18:25.711979"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.6"
+  #define VERSION_SHORT "v0.1.7"
 #endif
