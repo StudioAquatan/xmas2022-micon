f28c51d67fd5ccb38c8e70f2e1be9a0656d38e24
Hiroya-W <hyuyukun@gmail.com>
2022-12-15 13:08:22 +0900

    :hankey: Fix STAR_LEDS

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "7"
+  #define BUILD_NUMBER "8"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.7 - 2022-12-14 17:18:25.711979"
+  #define VERSION "v0.1.8 - 2022-12-15 13:07:56.106719"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.7"
+  #define VERSION_SHORT "v0.1.8"
 #endif
